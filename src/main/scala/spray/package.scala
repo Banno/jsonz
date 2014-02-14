3b5c8dbee7effa5e1f7f6078d458b51a69329e40
Luke Amdor <luke.amdor@gmail.com>
2014-02-14 15:42:27 -0600

    creating handy mixin trait for better

--- src/main/scala/spray/package.scala
+++ src/main/scala/spray/package.scala
@@ -4,23 +4,28 @@ import _root_.spray.httpx.marshalling._
 import _root_.spray.httpx.unmarshalling._
 
 package object spray {
-  implicit def writesMarshaller[T : Writes] =
-    Marshaller.of[T](ContentTypes.`application/json`) { (value, contentType, ctx) =>
-      ctx.marshalTo(HttpEntity(contentType, Jsonz.toJsonBytes(value)))
-    }
-
-
-  implicit def readsUnmarshaller[T : Reads]: Unmarshaller[T] =
-    new SimpleUnmarshaller[T] {
-      val canUnmarshalFrom: Seq[ContentTypeRange] = Seq(ContentTypeRange(MediaTypes.`application/json`))
-
-      def unmarshal(entity: HttpEntity) = entity match {
-        case body: HttpEntity if body.nonEmpty =>
-          val v = Jsonz.fromJsonBytes(body.data.toByteArray)
-          import DefaultFormats.nonEmptyListFormat
-          import DefaultFormats.jsFailureFormat
-          v.toEither.left.map(failures => MalformedContent(Jsonz.toJsonStr(failures)))
-        case _ => Left(ContentExpected)
+  trait JsonzMarshallers {
+    implicit def writesMarshaller[T : Writes] =
+      Marshaller.of[T](ContentTypes.`application/json`) { (value, contentType, ctx) =>
+        ctx.marshalTo(HttpEntity(contentType, Jsonz.toJsonBytes(value)))
       }
-    }
+
+
+    implicit def readsUnmarshaller[T : Reads]: Unmarshaller[T] =
+      new SimpleUnmarshaller[T] {
+        val canUnmarshalFrom: Seq[ContentTypeRange] = Seq(ContentTypeRange(MediaTypes.`application/json`))
+
+        def unmarshal(entity: HttpEntity) = entity match {
+          case body: HttpEntity if body.nonEmpty =>
+            val v = Jsonz.fromJsonBytes(body.data.toByteArray)
+            import DefaultFormats.nonEmptyListFormat
+            import DefaultFormats.jsFailureFormat
+            v.toEither.left.map(failures => MalformedContent(Jsonz.toJsonStr(failures)))
+          case _ => Left(ContentExpected)
+        }
+      }
+  }
+  object JsonzMarshallers extends JsonzMarshallers
+  implicit def writesMarshaller[T : Writes] = JsonzMarshallers.writesMarshaller
+  implicit def readsUnmarshaller[T : Reads] = JsonzMarshallers.readsUnmarshaller
 }
